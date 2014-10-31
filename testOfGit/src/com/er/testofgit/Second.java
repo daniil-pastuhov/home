89a434cdc3544e236eeda0934bc657a7f7bb7120
Daniil Pastuhov <daniil.pastuhov@gmail.com>
2014-10-31 12:30:16 +0300

    second commit

---
+++ testOfGit/src/com/er/testofgit/Second.java
@@ -0,0 +1,16 @@
+package com.er.testofgit;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Second extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+	}
+
+	
+	
+}
