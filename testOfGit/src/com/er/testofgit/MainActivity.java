0860c342d3ef5f21ae20d16e74bbbed3287f8833
Daniil Pastuhov <daniil.pastuhov@gmail.com>
2014-10-31 13:18:16 +0300

    idea commit 2

--- testOfGit/src/com/er/testofgit/MainActivity.java
+++ testOfGit/src/com/er/testofgit/MainActivity.java
@@ -2,15 +2,12 @@ package com.er.testofgit;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-TextView tv;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +19,7 @@ TextView tv;
 			@Override
 			public void onClick(View v) {
 				tv.setText("!!!!");
+                tv.setEnabled(false);
 			}
 		});
 	}
